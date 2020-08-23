2d4af0010762ecac54195c60d4cdc2a55657bad8
NGHIA (HOME-PC) <nghiango262@gmail.com>
2020-08-23 20:07:17 +0700

    test commit ee

--- my_commands.sh
+++ my_commands.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-project_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
 function create() {
-    echo "$project_path"
+    echo "$HOME"
     python $HOME/projects/caiquan/create.py
     
 }
