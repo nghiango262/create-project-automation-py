77b15b061430a695286e6c787a15332be629032a
NGHIA (HOME-PC) <nghiango262@gmail.com>
2020-08-23 20:03:42 +0700

    test 345

--- my_commands.sh
+++ my_commands.sh
@@ -4,8 +4,6 @@ project_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 function create() {
     echo "$project_path"
-    python $project_path/create.py
+    python $HOME/projects/${PWD##*/}/create.py
     
 }
-
-
